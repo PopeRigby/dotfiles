1d4f6aa24d56d81c2cbb160f1699c905d2cf3a76
PopeRigby <poperigby@mailbox.org>
2021-07-17 14:49:20 -0700

    Bootstrap Packer

--- dot_config/nvim/lua/plugins.lua
+++ dot_config/nvim/lua/plugins.lua
@@ -1,3 +1,15 @@
+-- Bootstrap Packer
+local execute = vim.api.nvim_command
+local fn = vim.fn
+
+local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+
+if fn.empty(fn.glob(install_path)) > 0 then
+  fn.system({'git', 'clone', 'https://github.com/wbthomason/packer.nvim', install_path})
+  execute 'packadd packer.nvim'
+end
+
+-- Define packages
 return require('packer').startup(function(use)
     use 'wbthomason/packer.nvim'
     use 'nvim-treesitter/nvim-treesitter'
