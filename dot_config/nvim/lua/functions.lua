8f64e2884b73e38cb2683fecfead2b3deea7f7d2
PopeRigby <poperigby@mailbox.org>
2021-07-20 16:26:25 -0700

    Cleanup functions.lua

--- dot_config/nvim/lua/functions.lua
+++ dot_config/nvim/lua/functions.lua
@@ -1,4 +1,30 @@
 function _G.split_term()
-    vim.api.nvim_command("split")
-    vim.api.nvim_command("terminal")
+   vim.api.nvim_command('split')
+end
+
+local t = function(str)
+  return vim.api.nvim_replace_termcodes(str, true, true, true)
+end
+
+local check_back_space = function()
+    local col = vim.fn.col('.') - 1
+    return col == 0 or vim.fn.getline('.'):sub(col, col):match('%s') ~= nil
+end
+
+-- Use (s-)tab to move to prev/next item in completion menuone
+_G.tab_complete = function()
+  if vim.fn.pumvisible() == 1 then
+    return t "<C-n>"
+  elseif check_back_space() then
+    return t "<Tab>"
+  else
+    return vim.fn['compe#complete']()
+  end
+end
+_G.s_tab_complete = function()
+  if vim.fn.pumvisible() == 1 then
+    return t "<C-p>"
+  else
+    return t "<S-Tab>"
+  end
 end
