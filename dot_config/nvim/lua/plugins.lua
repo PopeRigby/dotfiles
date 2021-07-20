8f71695de902209e82d929d210afd45de4f6b57b
PopeRigby <poperigby@mailbox.org>
2021-07-19 19:14:07 -0700

    Add some nice stuff to plugins.lua

--- dot_config/nvim/lua/plugins.lua
+++ dot_config/nvim/lua/plugins.lua
@@ -11,14 +11,19 @@ end
 
 -- Define packages
 return require('packer').startup({function(use)
+    use {
+        'hoob3rt/lualine.nvim',
+        requires = {'kyazdani42/nvim-web-devicons', opt = true}
+    }
+    use {
+        'nvim-treesitter/nvim-treesitter',
+        run = ':TSUpdate'
+    }
     use 'wbthomason/packer.nvim'
-    use 'nvim-treesitter/nvim-treesitter'
     use 'neovim/nvim-lspconfig'
     use 'hrsh7th/nvim-compe'
     use 'Mofiqul/dracula.nvim'
     use 'tpope/vim-commentary'
-    use 'hoob3rt/lualine.nvim'
-    use 'kyazdani42/nvim-web-devicons'
     use 'whiteinge/diffconflicts'
     use 'folke/zen-mode.nvim'
     use 'windwp/nvim-autopairs'
