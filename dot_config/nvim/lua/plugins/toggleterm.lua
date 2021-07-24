b7312754d4a93837bdb1f55970f5f58ba552a0d8
PopeRigby <poperigby@mailbox.org>
2021-07-24 11:47:39 -0700

    Remove nvim prefix from plugin config files

---
+++ dot_config/nvim/lua/plugins/toggleterm.lua
@@ -0,0 +1,7 @@
+require("toggleterm").setup {
+    open_mapping = '<c-T>',
+    hide_numbers = true, -- hide the number column in toggleterm buffers
+    shade_terminals = true,
+    start_in_insert = true,
+    insert_mappings = true, -- whether or not the open mapping applies in insert mode
+}
