c14cdbb853a99c181a0553fb8c2c3740c5e4b400
lumbo7332 <poperigby@mailbox.org>
2021-08-22 15:14:35 -0700

    Move all lsp stuff to lsp.lua

--- dot_config/nvim/lua/lsp.lua
+++ dot_config/nvim/lua/lsp.lua
@@ -1,3 +1,23 @@
+-- Setup LSP servers
+local lspinstall = require('lspinstall')
+local lspconfig = require('lspconfig')
+
+local function setup_servers()
+  lspinstall.setup()
+  local servers = lspinstall.installed_servers()
+  for _, server in pairs(servers) do
+    lspconfig[server].setup{}
+  end
+end
+
+setup_servers()
+
+-- Automatically reload after `:LspInstall <server>` so we don't have to restart neovim
+lspinstall.post_install_hook = function ()
+  setup_servers() -- reload installed servers
+  vim.cmd("bufdo e") -- this triggers the FileType autocmd that starts the server
+end
+
 -- Change diagnostic symbols in the sign column (gutter)
 local signs = { Error = " ", Warning = " ", Hint = " ", Information = " " }
 
@@ -12,3 +32,14 @@ vim.lsp.handlers['textDocument/publishDiagnostics'] = vim.lsp.with(vim.lsp.diagn
     signs = true,
     update_in_insert = true,
 })
+
+-- Individual LSP server configuration
+require('lspconfig').lua.setup {
+    settings = {
+        Lua = {
+            diagnostics = {
+                globals = { 'vim' }
+            }
+        }
+    }
+}
