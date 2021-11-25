e91663070df570523625d9e18d161787a3681df6
lumbo7332 <poperigby@mailbox.org>
2021-11-24 16:44:08 -0800

    Remove plasma-workspace

--- dot_config/plasma-workspace/env/executable_touchpad-fix.sh
+++
@@ -1,2 +0,0 @@
-xinput set-prop "PIXA3854:00 093A:0274 Touchpad" "libinput Click Method Enabled" 0 1
-
--- dot_config/plasma-workspace/env/executable_xdg-base-dir-fixes.sh
+++
@@ -1,4 +0,0 @@
-export XDG_CONFIG_HOME="~/.config"
-export XDG_CONFIG_HOME="~/.local/share"
-export GTK2_RC_FILES="$XDG_CONFIG_HOME/gtk-2.0/gtkrc"
-export CARGO_HOME="$XDG_DATA_HOME/cargo"
