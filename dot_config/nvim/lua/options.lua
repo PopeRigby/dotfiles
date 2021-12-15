521eecb88b4f0a5b2fd1f3e732b6764937bbb240
lumbo7332 <poperigby@mailbox.org>
2021-12-15 15:52:32 -0800

    Update options file

--- dot_config/nvim/lua/options.lua
+++ dot_config/nvim/lua/options.lua
@@ -1,36 +1,45 @@
 local cmd = vim.cmd
-local opt = vim.opt
 local var = vim.g
 
 -- Miscellaneous options
-opt.completeopt = {"menuone", "noinsert", "noselect"} -- Set completeopt to have a better completion experience
-opt.expandtab = true -- Use spaces instead of tabs
-opt.tabstop = 4 -- Number of spaces tabs count for
-opt.shiftwidth = 4 -- Size of an indent
-opt.cursorline = true -- Highlight cursor line
-opt.timeoutlen = 2000 -- Lengthen leader key timeout
-opt.termguicolors = true -- True color support
-opt.ignorecase = true -- Ignore case while searching
-opt.smartcase = true -- Do not ignore case with capitals while searching
-opt.splitbelow = true -- Put new windows below current
-opt.splitright = true -- Put new windows right of curren
-opt.updatetime = 250 -- Reduce updatetime which affects CursorHold
-opt.autoindent = true -- Copy indent from current line when starting a new line
-opt.smartindent = true -- Do smart autoindenting when starting a new line
-opt.scrolloff = 2 -- Lines of context
-opt.lazyredraw = true -- Don't redraw screen when running macros/regex on large files
-opt.hidden = true -- Don't abandon hidden buffers
-opt.foldmethod = "expr"
-opt.foldexpr = "nvim_treesitter#foldexpr()"
-opt.foldlevelstart = 99
+local options = {
+    completeopt = { "menuone", "noinsert", "noselect" }, -- Set completeopt to have a better completion experience
+    undofile = true, -- Enable persistent undo
+    signcolumn = "yes", -- always show the sign column, otherwise it would shift the text each time
+    expandtab = true, -- Use spaces instead of tabs
+    tabstop = 4, -- Number of spaces tabs count for
+    shiftwidth = 4, -- Size of an indent
+    cursorline = true, -- Highlight cursor line
+    timeoutlen = 2000, -- Lengthen leader key timeout
+    termguicolors = true, -- True color support
+    ignorecase = true, -- Ignore case while searching
+    mouse = "a", -- Enable mouse support
+    showmode = false, -- we don't need to see things like -- INSERT -- anymore
+    smartcase = true, -- Do not ignore case with capitals while searching
+    splitbelow = true, -- Put new windows below current
+    splitright = true, -- Put new windows right of curren
+    updatetime = 250, -- Reduce updatetime which affects CursorHold
+    autoindent = true, -- Copy indent from current line when starting a new line
+    smartindent = true, -- Do smart autoindenting when starting a new line
+    scrolloff = 2, -- Lines of context
+    lazyredraw = true, -- Don't redraw screen when running macros/regex on large files
+    hidden = true, -- Don't abandon hidden buffers
+    foldmethod = "expr",
+    foldexpr = "nvim_treesitter#foldexpr()",
+    foldlevelstart = 99,
+}
+
+for k, v in pairs(options) do
+    vim.opt[k] = v
+end
 
 -- Variables
 var.mapleader = ";"
 
 -- Autocommands
-cmd "au TextYankPost * lua vim.highlight.on_yank {on_visual = false}" -- Highlight text briefly after yank
-cmd "au BufWritePost ~/.local/share/chezmoi/* silent ! chezmoi apply --source-path %" -- Automatically run `chezmoi apply` on save
-cmd "au CursorHold,CursorHoldI * lua vim.lsp.diagnostic.show_line_diagnostics({focusable=false})" -- Show line diagnostics automatically in hover window
+cmd("au TextYankPost * lua vim.highlight.on_yank {on_visual = false}") -- Highlight text briefly after yank
+cmd("au BufWritePost ~/.local/share/chezmoi/* silent ! chezmoi apply --source-path %") -- Automatically run `chezmoi apply` on save
+cmd("au CursorHold,CursorHoldI * lua vim.lsp.diagnostic.show_line_diagnostics({focusable=false})") -- Show line diagnostics automatically in hover window
 
 -- Disable built-in plugins
 local disabled_built_ins = {
@@ -51,7 +60,7 @@ local disabled_built_ins = {
     "logipat",
     "rrhelper",
     "spellfile_plugin",
-    "matchit"
+    "matchit",
 }
 
 for _, plugin in pairs(disabled_built_ins) do
