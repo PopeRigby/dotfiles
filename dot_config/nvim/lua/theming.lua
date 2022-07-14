701c95a6592b9beecc5dbca53985665368cf91e6
Pope_Rigby <poperigby@mailbox.org>
2022-07-13 17:36:32 -0700

    Change some nvim-cmp stuff

--- dot_config/nvim/lua/theming.lua
+++ dot_config/nvim/lua/theming.lua
@@ -1,8 +1,32 @@
-local cmd = vim.cmd
+local highlight = function(group, fg, bg, attr, sp)
+    fg = fg and "guifg=" .. fg or "guifg=NONE"
+    bg = bg and "guibg=" .. bg or "guibg=NONE"
+    attr = attr and "gui=" .. attr or "gui=NONE"
+    sp = sp and "guisp=" .. sp or ""
 
-cmd("colorscheme dracula")
-cmd("highlight Normal guibg=NONE")
-cmd("highlight SignColumn guibg=NONE")
-cmd("highlight NormalFloat guibg=#282a36")
+    vim.api.nvim_command("highlight " .. group .. " " .. fg .. " " .. bg .. " " .. attr .. " " .. sp)
+end
 
--- cmd("autocmd ColorScheme * highlight CompeDocumentation guibg=#282a36")
+vim.g.dracula_transparent_bg = true
+
+vim.cmd("colorscheme dracula")
+
+-- nvim-cmp
+local colors = require("dracula").colors()
+
+-- gray
+highlight("CmpItemAbbrDeprecated", colors.visual, nil, "strikethrough")
+-- cyan
+highlight("CmpItemAbbrMatch", colors.cyan, nil, nil, nil)
+highlight("CmpItemAbbrMatchFuzzy", colors.cyan, nil, nil, nil)
+-- bright cyan
+highlight("CmpItemKindVariable", colors.bright_cyan, nil, nil, nil)
+highlight("CmpItemKindInterface", colors.bright_cyan, nil, nil, nil)
+highlight("CmpItemKindText", colors.bright_cyan, nil, nil, nil)
+-- pink
+highlight("CmpItemKindFunction", colors.pink, nil, nil, nil)
+highlight("CmpItemKindMethod", colors.pink, nil, nil, nil)
+-- white
+highlight("CmpItemKindKeyword", colors.fg, nil, nil, nil)
+highlight("CmpItemKindProperty", colors.fg, nil, nil, nil)
+highlight("CmpItemKindUnit", colors.fg, nil, nil, nil)
--- dot_config/nvim/plugin/cmp.lua
+++ dot_config/nvim/plugin/cmp.lua
@@ -57,7 +57,7 @@ cmp.setup({
         }),
     },
     view = {
-        entries = "native",
+        entries = "custom",
     },
     sources = {
         { name = "nvim_lsp", "buffer", "path" },
@@ -81,7 +81,17 @@ cmp.setup({
             return vim_item
         end,
     },
+    enabled = function()
+        -- disable completion in comments
+        local context = require("cmp.config.context")
+        -- keep command mode completion enabled when cursor is in a comment
+        if vim.api.nvim_get_mode().mode == "c" then
+            return true
+        else
+            return not context.in_treesitter_capture("comment") and not context.in_syntax_group("Comment")
+        end
+    end,
     experimental = {
-        ghost_text = false,
+        ghost_text = true,
     },
 })
