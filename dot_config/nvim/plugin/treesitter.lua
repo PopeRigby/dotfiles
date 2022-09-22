70707c40ccd3ff8439b59a93270f1f9631015a43
Pope_Rigby <poperigby@mailbox.org>
2022-09-22 13:53:16 -0700

    Automatically install treesitter servers

--- dot_config/nvim/plugin/treesitter.lua
+++ dot_config/nvim/plugin/treesitter.lua
@@ -1,11 +1,12 @@
-require "nvim-treesitter.configs".setup {
-    autopairs = {
-        enable = true
-    },
-    highlight = {
-        enable = true
-    },
-    indent = {
-        enable = false
-    }
-}
+require("nvim-treesitter.configs").setup({
+	auto_install = true,
+	autopairs = {
+		enable = true,
+	},
+	highlight = {
+		enable = true,
+	},
+	indent = {
+		enable = false,
+	},
+})
