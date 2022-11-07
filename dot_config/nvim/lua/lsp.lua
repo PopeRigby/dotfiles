ea14d63098963e9f1ed399d275944146464893a4
Pope_Rigby <poperigby@mailbox.org>
2022-11-07 15:41:52 -0800

    Add fix for offset encoding error

--- dot_config/nvim/lua/lsp.lua
+++ dot_config/nvim/lua/lsp.lua
@@ -73,3 +73,8 @@ null_ls.setup({
 		end
 	end,
 })
+
+-- Fix for multiple offset encoding error
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+capabilities.offsetEncoding = { "utf-16" }
+require("lspconfig").clangd.setup({ capabilities = capabilities })
