792b00d357ad2efb2687fb5d2137c3de97f4c633
Pope_Rigby <poperigby@mailbox.org>
2022-11-09 11:52:37 -0800

    Use LS for formatting if available

--- dot_config/nvim/lua/lsp.lua
+++ dot_config/nvim/lua/lsp.lua
@@ -2,7 +2,6 @@ local mason = require("mason")
 local null_ls = require("null-ls")
 local mason_null_ls = require("mason-null-ls")
 local lspconfig = require("lspconfig")
-local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 
 -- Change diagnostic symbols in the sign column (gutter)
 local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " }
@@ -11,17 +10,33 @@ for type, icon in pairs(signs) do
 	vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
 end
 
--- Mason setup
+-- Format on save function
+local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+local on_attach =
+	function(client, bufnr)
+		if client.supports_method("textDocument/formatting") then
+			vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+			vim.api.nvim_create_autocmd("BufWritePre", {
+				group = augroup,
+				buffer = bufnr,
+				callback = function()
+					vim.lsp.buf.format({ bufnr = bufnr })
+				end,
+			})
+		end
+	end,
+	
+	-- Mason setup
 mason.setup({})
 -- Automatically setup servers installed with mason.nvim
 require("mason-lspconfig").setup_handlers({
-	-- The first entry (without a key) will be the default handler
-	-- and will be called for each installed server that doesn't have
-	-- a dedicated handler.
-	function(server_name) -- default handler (optional)
-		lspconfig[server_name].setup({})
+	-- Default handler
+	function(server_name)
+		lspconfig[server_name].setup({
+			on_attach = on_attach,
+		})
 	end,
-	-- Next, you can provide targeted overrides for specific servers.
+	-- Targetted overrides for specific servers
 	["sumneko_lua"] = function()
 		lspconfig.sumneko_lua.setup({
 			settings = {
@@ -35,12 +50,6 @@ require("mason-lspconfig").setup_handlers({
 	end,
 })
 
--- Mason DAP
--- require("dap").setup()
-require("mason-nvim-dap").setup({
-	automatic_setup = true,
-})
-
 -- Mason null-ls
 mason_null_ls.setup()
 mason_null_ls.setup_handlers({
@@ -49,29 +58,13 @@ mason_null_ls.setup_handlers({
 	end,
 })
 
+-- null-ls
 null_ls.setup({
 	sources = {
 		null_ls.builtins.formatting.clang_format,
 		null_ls.builtins.formatting.stylua,
 	},
-	-- Format on save
-	on_attach = function(client, bufnr)
-		if client.supports_method("textDocument/formatting") then
-			vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-			vim.api.nvim_create_autocmd("BufWritePre", {
-				group = augroup,
-				buffer = bufnr,
-				callback = function()
-					vim.lsp.buf.format({
-						bufnr = bufnr,
-						filter = function(client)
-							return client.name == "null-ls"
-						end,
-					})
-				end,
-			})
-		end
-	end,
+	on_attach = on_attach,
 })
 
 -- Fix for multiple offset encoding error
