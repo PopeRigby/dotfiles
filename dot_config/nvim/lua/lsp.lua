dd7ecb23478d8c2b07febd70ce82942bf9c1e493
Pope_Rigby <poperigby@mailbox.org>
2022-11-09 12:01:29 -0800

    Fix some stuff

--- dot_config/nvim/lua/lsp.lua
+++ dot_config/nvim/lua/lsp.lua
@@ -23,8 +23,8 @@ local on_attach =
 		end
 	end,
 	
-	-- Mason setup
-mason.setup({})
+	-- Mason
+require("mason").setup({})
 -- Automatically setup servers installed with mason.nvim
 require("mason-lspconfig").setup_handlers({
 	-- Default handler
@@ -47,15 +47,8 @@ require("mason-lspconfig").setup_handlers({
 	end,
 })
 
--- Mason null-ls
-mason_null_ls.setup()
-mason_null_ls.setup_handlers({
-	function(source_name)
-		-- all sources with no handler get passed here
-	end,
-})
-
 -- null-ls
+local null_ls = require("null-ls")
 null_ls.setup({
 	sources = {
 		null_ls.builtins.formatting.clang_format,
@@ -68,9 +61,8 @@ null_ls.setup({
 require("mason-null-ls").setup({
 	automatic_setup = true,
 })
--- require("mason-null-ls").setup_handlers()
 
 -- Fix for multiple offset encoding error
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities.offsetEncoding = { "utf-16" }
-require("lspconfig").clangd.setup({ capabilities = capabilities })
+lspconfig.clangd.setup({ capabilities = capabilities })
