6c19bae1bea1399f9b45ea1adf256f95dc885f57
Pope_Rigby <poperigby@mailbox.org>
2022-11-20 17:42:53 -0800

    Make VertSplit gray

--- dot_config/nvim/lua/theming.lua
+++ dot_config/nvim/lua/theming.lua
@@ -2,6 +2,9 @@ local dracula = require("dracula")
 
 dracula.setup({
 	transparent_bg = true,
+	overrides = {
+		VertSplit = { fg = dracula.colors().selection },
+	},
 })
 
 vim.cmd("colorscheme dracula")
