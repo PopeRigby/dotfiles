9fd252b00e8dc8919136c98a3847adac2bf8bbc6
Pope_Rigby <poperigby@mailbox.org>
2022-11-22 14:34:56 -0800

    Update alpha buttons

--- dot_config/nvim/plugin/alpha.lua
+++ dot_config/nvim/plugin/alpha.lua
@@ -15,12 +15,12 @@ dashboard.section.header.val = {
 
 -- Set menu
 dashboard.section.buttons.val = {
-	dashboard.button("n", "  > New file", ":ene <BAR> startinsert <CR>"),
-	dashboard.button("f", "  > Find file", ":cd $HOME/Projects | Telescope find_files<CR>"),
-	dashboard.button("r", "  > Recent files", ":Telescope oldfiles<CR>"),
-	dashboard.button("u", "  > Update plugins", ":PackerSync<CR>"),
-	dashboard.button("h", "ﬤ  > Help", ":Telescope help_tags<CR>"),
-	dashboard.button("q", "  > Quit NVIM", ":qa<CR>"),
+	dashboard.button("n", "  New file", ":ene <BAR> startinsert <CR>"),
+	dashboard.button("f", "  Find file", ":cd $HOME/Projects | Telescope find_files<CR>"),
+	dashboard.button("r", "  Recent files", ":Telescope oldfiles<CR>"),
+	dashboard.button("u", "  Update plugins", ":PackerSync<CR>"),
+	dashboard.button("h", "ﬤ  Help", ":Telescope help_tags<CR>"),
+	dashboard.button("q", "  Quit NVIM", ":qa<CR>"),
 }
 
 -- Set footer
