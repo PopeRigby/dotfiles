97620eefc0b1c647f4a7d75b181abf5d127a1524
Pope_Rigby <poperigby@mailbox.org>
2022-11-29 18:18:27 -0800

    Get keybinding working (I think)

--- dot_config/nvim/plugin/cmp.lua
+++ dot_config/nvim/plugin/cmp.lua
@@ -32,28 +32,43 @@ local kind_icons = {
 }
 
 local cmp = require("cmp")
+local luasnip = require("luasnip")
 cmp.setup({
-	mapping = {
+	mapping = cmp.mapping.preset.insert({
+		["<C-d>"] = cmp.mapping.scroll_docs(-4),
+		["<C-f>"] = cmp.mapping.scroll_docs(4),
+		["<C-Space>"] = cmp.mapping.complete(),
+		["<CR>"] = cmp.mapping.confirm({
+			behavior = cmp.ConfirmBehavior.Replace,
+			select = true,
+		}),
 		["<Tab>"] = cmp.mapping(function(fallback)
 			if cmp.visible() then
 				cmp.select_next_item()
-			elseif has_words_before() then
-				cmp.complete()
+			elseif luasnip.expand_or_jumpable() then
+				luasnip.expand_or_jump()
 			else
 				fallback()
 			end
 		end, { "i", "s" }),
-
 		["<S-Tab>"] = cmp.mapping(function(fallback)
 			if cmp.visible() then
 				cmp.select_prev_item()
+			elseif luasnip.jumpable(-1) then
+				luasnip.jump(-1)
 			else
 				fallback()
 			end
 		end, { "i", "s" }),
+	}),
+	sources = {
+		{ name = "nvim_lsp" },
+		{ name = "luasnip" },
 	},
-	view = {
-		entries = "custom",
+	snippet = {
+		expand = function(args)
+			require("luasnip").lsp_expand(args.body)
+		end,
 	},
 	sources = {
 		{ name = "nvim_lsp" },
@@ -61,6 +76,10 @@ cmp.setup({
 		{ name = "syntax" },
 		{ name = "buffer" },
 		{ name = "path" },
+		{ name = "luasnip" },
+	},
+	view = {
+		entries = "custom",
 	},
 	formatting = { -- Use pretty icons
 		format = function(entry, vim_item)
