063e24aa21ac3dbc6e40d4e1ae1a737adb040bc1
Pope_Rigby <poperigby@mailbox.org>
2022-11-29 18:24:49 -0800

    Fix stuff up

--- dot_config/nvim/plugin/cmp.lua
+++ dot_config/nvim/plugin/cmp.lua
@@ -37,7 +37,6 @@ cmp.setup({
 	mapping = cmp.mapping.preset.insert({
 		["<C-d>"] = cmp.mapping.scroll_docs(-4),
 		["<C-f>"] = cmp.mapping.scroll_docs(4),
-		["<C-Space>"] = cmp.mapping.complete(),
 		["<CR>"] = cmp.mapping.confirm({
 			behavior = cmp.ConfirmBehavior.Replace,
 			select = true,
@@ -61,10 +60,6 @@ cmp.setup({
 			end
 		end, { "i", "s" }),
 	}),
-	sources = {
-		{ name = "nvim_lsp" },
-		{ name = "luasnip" },
-	},
 	snippet = {
 		expand = function(args)
 			require("luasnip").lsp_expand(args.body)
