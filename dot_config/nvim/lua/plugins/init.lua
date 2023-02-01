788f6db0f17a2e623e613b89e687fe5f95ca0c32
Pope_Rigby <poperigby@mailbox.org>
2023-01-31 18:23:13 -0800

    Move more plugins to files

--- dot_config/nvim/lua/plugins/init.lua
+++ dot_config/nvim/lua/plugins/init.lua
@@ -12,43 +12,4 @@ return {
     "jayp0521/mason-null-ls.nvim",
     "mfussenegger/nvim-dap",
     "jayp0521/mason-nvim-dap.nvim",
-    {
-        "nvim-telescope/telescope.nvim",
-        dependencies = { "nvim-lua/popup.nvim" },
-    },
-    {
-        "numToStr/Comment.nvim",
-        config = function()
-            require("Comment").setup()
-        end,
-    },
-    {
-        "folke/trouble.nvim",
-        config = function()
-            require("trouble").setup()
-        end,
-    },
-    {
-        "kylechui/nvim-surround",
-        config = function()
-            require("nvim-surround").setup()
-        end,
-    },
-    {
-        "nvim-zh/colorful-winsep.nvim",
-        config = function()
-            require("colorful-winsep").setup()
-        end,
-    },
-    {
-        "goolord/alpha-nvim",
-        dependencies = { "nvim-tree/nvim-web-devicons" },
-    },
-    {
-        "folke/todo-comments.nvim",
-        requires = "nvim-lua/plenary.nvim",
-        config = function()
-            require("todo-comments").setup()
-        end,
-    }
 }
