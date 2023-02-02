2fc6e24e2270836151b0fb907991cefc16753972
Pope_Rigby <poperigby@mailbox.org>
2023-02-02 14:22:03 -0800

    Add qmlls configuration

--- dot_config/nvim/lua/lsp.lua
+++ dot_config/nvim/lua/lsp.lua
@@ -3,72 +3,78 @@ local lspconfig = require("lspconfig")
 -- Change diagnostic symbols in the sign column (gutter)
 local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " }
 for type, icon in pairs(signs) do
-	local hl = "DiagnosticSign" .. type
-	vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
+    local hl = "DiagnosticSign" .. type
+    vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
 end
 
 -- Format on save function
 local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 local on_attach =
-	function(client, bufnr)
-		if client.supports_method("textDocument/formatting") then
-			vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-			vim.api.nvim_create_autocmd("BufWritePre", {
-				group = augroup,
-				buffer = bufnr,
-				callback = function()
-					vim.lsp.buf.format({ bufnr = bufnr })
-				end,
-			})
-		end
-	end,
-	
-	-- Mason
-require("mason").setup()
+function(client, bufnr)
+    if client.supports_method("textDocument/formatting") then
+        vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+        vim.api.nvim_create_autocmd("BufWritePre", {
+            group = augroup,
+            buffer = bufnr,
+            callback = function()
+                vim.lsp.buf.format({ bufnr = bufnr })
+            end,
+        })
+    end
+end,
+
+    -- Mason
+    require("mason").setup()
 -- Automatically setup servers installed with mason.nvim
 require("mason-lspconfig").setup_handlers({
-	-- Default handler
-	function(server_name)
-		lspconfig[server_name].setup({
-			on_attach = on_attach,
-		})
-	end,
-	-- Targetted overrides for specific servers
-	["sumneko_lua"] = function()
-		lspconfig.sumneko_lua.setup({
-			settings = {
-				Lua = {
-					diagnostics = {
-						globals = { "vim", "minetest" },
-					},
-				},
-			},
-			on_attach = on_attach,
-		})
-	end,
-	["clangd"] = function()
-		-- Fix for multiple offset encoding error
-		local capabilities = vim.lsp.protocol.make_client_capabilities()
-		capabilities.offsetEncoding = { "utf-16" }
-		lspconfig.clangd.setup({
-			capabilities = capabilities,
-			on_attach = on_attach,
-		})
-	end,
+    -- Default handler
+    function(server_name)
+        lspconfig[server_name].setup({
+            on_attach = on_attach,
+        })
+    end,
+    -- Targetted overrides for specific servers
+    ["sumneko_lua"] = function()
+        lspconfig.sumneko_lua.setup({
+            settings = {
+                Lua = {
+                    diagnostics = {
+                        globals = { "vim", "minetest" },
+                    },
+                },
+            },
+            on_attach = on_attach,
+        })
+    end,
+    ["clangd"] = function()
+        -- Fix for multiple offset encoding error
+        local capabilities = vim.lsp.protocol.make_client_capabilities()
+        capabilities.offsetEncoding = { "utf-16" }
+        lspconfig.clangd.setup({
+            capabilities = capabilities,
+            on_attach = on_attach,
+        })
+    end,
 })
 
 -- null-ls
 require("null-ls").setup({
-	on_attach = on_attach,
+    on_attach = on_attach,
 })
 
 -- Mason null-ls
 require("mason-null-ls").setup({
-	automatic_setup = true,
+    automatic_setup = true,
 })
 require("mason-null-ls").setup_handlers()
 
 require("mason-nvim-dap").setup({
-	automatic_setup = true,
+    automatic_setup = true,
 })
 require("mason-nvim-dap").setup_handlers()
+
+-- Language servers not handled by Mason
+require("lspconfig").qmlls.setup({
+    cmd = { "qmlls6" },
+    filetypes = { "qml" }
+})
