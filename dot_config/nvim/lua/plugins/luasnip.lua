71462a9d69ab4a23b13d2248e8de0259f0cf995f
Pope_Rigby <poperigby@mailbox.org>
2023-02-07 10:25:10 -0800

    Update plugins

--- dot_config/nvim/lua/plugins/luasnip.lua
+++ dot_config/nvim/lua/plugins/luasnip.lua
@@ -1,8 +1,8 @@
 return {
-	"L3MON4D3/LuaSnip",
-	-- version = "<CurrentMajor>.*",
-	dependencies = { "rafamadriz/friendly-snippets" },
-	config = function()
-		require("luasnip.loaders.from_vscode").lazy_load()
-	end
+    "L3MON4D3/LuaSnip",
+    -- version = "<CurrentMajor>.*",
+    dependencies = { "rafamadriz/friendly-snippets" },
+    config = function()
+        require("luasnip.loaders.from_vscode").lazy_load()
+    end
 }
