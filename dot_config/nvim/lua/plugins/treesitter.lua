8a9a221b2367b59429dfbcf96d46c05f6bbea9c7
Pope_Rigby <poperigby@mailbox.org>
2023-02-09 13:48:53 -0800

    Fix treesitter config

--- dot_config/nvim/lua/plugins/treesitter.lua
+++ dot_config/nvim/lua/plugins/treesitter.lua
@@ -1,16 +1,18 @@
 return {
     "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate",
-    opts = {
-        auto_install = true,
-        autopairs = {
-            enable = true,
-        },
-        highlight = {
-            enable = true,
-        },
-        indent = {
-            enable = false,
-        },
-    }
+    config = function()
+        require("nvim-treesitter.configs").setup({
+            auto_install = true,
+            autopairs = {
+                enable = true,
+            },
+            highlight = {
+                enable = true,
+            },
+            indent = {
+                enable = false,
+            },
+        })
+    end
 }
