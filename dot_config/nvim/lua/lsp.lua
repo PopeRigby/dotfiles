fe2b745091ccaaa56ede95eae9fa299b1b5af343
Pope_Rigby <poperigby@mailbox.org>
2023-03-02 10:27:44 -0800

    Get nvim-ufo working

--- dot_config/nvim/lua/lsp.lua
+++ dot_config/nvim/lua/lsp.lua
@@ -10,18 +10,18 @@ end
 -- Format on save function
 local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 local on_attach =
-function(client, bufnr)
-    if client.supports_method("textDocument/formatting") then
-        vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-        vim.api.nvim_create_autocmd("BufWritePre", {
-            group = augroup,
-            buffer = bufnr,
-            callback = function()
-                vim.lsp.buf.format({ bufnr = bufnr })
-            end,
-        })
-    end
-end,
+    function(client, bufnr)
+        if client.supports_method("textDocument/formatting") then
+            vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+            vim.api.nvim_create_autocmd("BufWritePre", {
+                group = augroup,
+                buffer = bufnr,
+                callback = function()
+                    vim.lsp.buf.format({ bufnr = bufnr })
+                end,
+            })
+        end
+    end,
 
     -- Mason
     require("mason").setup()
--- dot_config/nvim/lua/plugins/ufo.lua
+++ dot_config/nvim/lua/plugins/ufo.lua
@@ -1,22 +1,23 @@
 return {
     "kevinhwang91/nvim-ufo",
-    dependencies = "kevinhwang91/promise-async",
-    -- keys = {
-    --     {
-    --         "<Space>h",
-    --         function()
-    --             -- Either preview fold, or LSP hover
-    --             local winid = require("ufo").peekFoldedLinesUnderCursor()
-    --             if not winid then
-    --                 vim.lsp.buf.hover()
-    --             end
-    --         end,
-    --         desc = "Preview fold or LSP hover"
-    --     },
-    -- },
-    opt = {
-        provider_selector = function(bufnr, filetype, buftype)
-            return { "treesitter", "indent" }
-        end,
-    }
+    dependencies = { "kevinhwang91/promise-async" },
+    event = "BufRead",
+    keys = {
+        { "zR", function() require("ufo").openAllFolds() end },
+        { "zM", function() require("ufo").closeAllFolds() end },
+        { "<Space>h", function()
+            local winid = require('ufo').peekFoldedLinesUnderCursor()
+            if not winid then
+                vim.lsp.buf.hover()
+            end
+        end }
+    },
+    config = function()
+        require("ufo").setup({
+            close_fold_kinds = { "imports" },
+            provider_selector = function(bufnr, filetype, buftype)
+                return { 'treesitter', 'indent' }
+            end
+        })
+    end,
 }
