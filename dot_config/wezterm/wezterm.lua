4f5267e2d3519368abdf04b0b52ca96d7a126ae0
Pope_Rigby <poperigby@mailbox.org>
2023-08-19 19:45:24 -0700

    Add back wezterm shortcuts

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -35,9 +35,19 @@ local settings = {
     },
     keys = {
         { key = "t", mods = "ALT", action = wezterm.action({ SpawnTab = "DefaultDomain" }) },
-        -- ... (repeat for other keys)
+        { key = "w", mods = "ALT", action = wezterm.action({ CloseCurrentTab = { confirm = true } }) },
+        { key = "1", mods = "ALT", action = wezterm.action({ ActivateTab = 0 }) },
+        { key = "2", mods = "ALT", action = wezterm.action({ ActivateTab = 1 }) },
+        { key = "3", mods = "ALT", action = wezterm.action({ ActivateTab = 2 }) },
+        { key = "4", mods = "ALT", action = wezterm.action({ ActivateTab = 3 }) },
+        { key = "5", mods = "ALT", action = wezterm.action({ ActivateTab = 4 }) },
+        { key = "6", mods = "ALT", action = wezterm.action({ ActivateTab = 5 }) },
+        { key = "7", mods = "ALT", action = wezterm.action({ ActivateTab = 6 }) },
+        { key = "8", mods = "ALT", action = wezterm.action({ ActivateTab = 7 }) },
+        { key = "9", mods = "ALT", action = wezterm.action({ ActivateTab = 8 }) },
     },
     color_scheme = "Gruvbox dark, soft (base16)",
+
 }
 
 -- Copy the settings from the settings table to the config table
