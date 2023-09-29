9ef6f80bb2e573d8fac0b5639a0eebf9f0e289e9
Pope_Rigby <poperigby@mailbox.org>
2023-09-29 11:42:29 -0700

    neovim: Make NormalFloat not transparent

--- dot_config/nvim/lua/theming.lua
+++ dot_config/nvim/lua/theming.lua
@@ -2,6 +2,10 @@ vim.o.background = "dark"
 
 require("gruvbox").setup({
     transparent_mode = true,
+    overrides = {
+        -- Make float backgrounds darker (and not transparent)
+        NormalFloat = {bg = "#282828"}
+    }
 })
 
 vim.cmd("colorscheme gruvbox")
