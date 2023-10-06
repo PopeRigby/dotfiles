8555a0e08ff346c4a1d224e79e383afc59ab541c
Pope_Rigby <poperigby@mailbox.org>
2023-10-06 14:10:17 -0700

    fish: move aliases to aliases.fish

---
+++ dot_config/fish/aliases.fish
@@ -0,0 +1,40 @@
+# Set aliases if programs are present
+if type -q exa
+    alias ls "exa"
+end
+
+if type -q bat
+    alias cat "bat"
+end
+
+if type -q python
+    alias py "python"
+end
+
+if type -q lazygit
+    alias lg "lazygit"
+end
+
+if type -q distrobox
+    alias db "distrobox"
+end
+
+if type -q borgmatic
+    alias borgmatic "sudo borgmatic"
+end
+
+if type -q systemctl
+    alias sys "sudo systemctl"
+    alias syu "systemctl --user"
+end
+
+if type -q journalctl
+    alias jrn "sudo journalctl"
+end
+
+if type -q rpm-ostree
+    alias ros "rpm-ostree"
+end
+
+# Miscellaneous aliases
+alias haddock "ssh cassidy@192.168.1.184"
