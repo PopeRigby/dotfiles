84413d67d50f2cdf337bbe45072723c01f82f82d
Pope_Rigby <poperigby@mailbox.org>
2023-10-09 14:10:37 -0700

    Use spaces instead of tabs

--- dot_config/nvim/lua/plugins/init.lua
+++ dot_config/nvim/lua/plugins/init.lua
@@ -1,10 +1,10 @@
 return {
-	"neovim/nvim-lspconfig",
-	"stevearc/dressing.nvim",
-	"williamboman/mason.nvim",
-	"williamboman/mason-lspconfig.nvim",
-	"jose-elias-alvarez/null-ls.nvim",
-	"jayp0521/mason-null-ls.nvim",
-	"mfussenegger/nvim-dap",
-	"jayp0521/mason-nvim-dap.nvim",
+    "neovim/nvim-lspconfig",
+    "stevearc/dressing.nvim",
+    "williamboman/mason.nvim",
+    "williamboman/mason-lspconfig.nvim",
+    "jose-elias-alvarez/null-ls.nvim",
+    "jayp0521/mason-null-ls.nvim",
+    "mfussenegger/nvim-dap",
+    "jayp0521/mason-nvim-dap.nvim",
 }
--- dot_config/nvim/lua/plugins/telescope.lua
+++ dot_config/nvim/lua/plugins/telescope.lua
@@ -1,32 +1,32 @@
 return {
-	"nvim-telescope/telescope.nvim",
-	dependencies = {
-		"nvim-lua/popup.nvim",
-		"nvim-tree/nvim-web-devicons",
-		{
-			"nvim-telescope/telescope-fzf-native.nvim",
-			build = "make",
-		},
-	},
-	opts = function()
-		require("telescope").setup()
-		require("telescope").load_extension("fzf")
-	end,
-	keys = {
-		{
-			"<Leader>t",
-			":Telescope <CR>",
-			desc = "Opens Telescope",
-		},
-		{
-			"<Leader>e",
-			":Telescope find_files <CR>",
-			desc = "Explore files in Telescope",
-		},
-		{
-			"<Leader>b",
-			":Telescope buffers <CR>",
-			desc = "Explore buffers in Telescope",
-		},
-	},
+    "nvim-telescope/telescope.nvim",
+    dependencies = {
+        "nvim-lua/popup.nvim",
+        "nvim-tree/nvim-web-devicons",
+        {
+            "nvim-telescope/telescope-fzf-native.nvim",
+            build = "make",
+        },
+    },
+    opts = function()
+        require("telescope").setup()
+        require("telescope").load_extension("fzf")
+    end,
+    keys = {
+        {
+            "<Leader>t",
+            ":Telescope <CR>",
+            desc = "Opens Telescope",
+        },
+        {
+            "<Leader>e",
+            ":Telescope find_files <CR>",
+            desc = "Explore files in Telescope",
+        },
+        {
+            "<Leader>b",
+            ":Telescope buffers <CR>",
+            desc = "Explore buffers in Telescope",
+        },
+    },
 }
