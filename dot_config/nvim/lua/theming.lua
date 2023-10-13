012f7c0c72e0f055f4f74ec9c5beb7aba1b053ea
Pope_Rigby <poperigby@mailbox.org>
2023-10-12 19:36:43 -0700

    neovim: make incorrectly spelled words have a red undercurl

--- dot_config/nvim/lua/theming.lua
+++ dot_config/nvim/lua/theming.lua
@@ -1,11 +1,13 @@
 vim.o.background = "dark"
 
 require("gruvbox").setup({
-	transparent_mode = true,
-	overrides = {
-		-- Make float backgrounds darker (and not transparent)
-		NormalFloat = { bg = "#282828" },
-	},
+    transparent_mode = true,
+    overrides = {
+        -- Make float backgrounds darker (and not transparent)
+        NormalFloat = { bg = "#282828" },
+        -- Make incorrectly spelled words have a curly red underline
+        SpellBad = { undercurl = true, sp = "#fb4934" },
+    },
 })
 
 vim.cmd("colorscheme gruvbox")
