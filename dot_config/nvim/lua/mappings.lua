d8261de6327b64809132156491d1554e1f4ee54c
Pope_Rigby <poperigby@mailbox.org>
2023-10-16 18:44:28 -0700

    neovim: dd descriptions for some bindings

--- dot_config/nvim/lua/mappings.lua
+++ dot_config/nvim/lua/mappings.lua
@@ -1,13 +1,14 @@
 local map = vim.keymap.set
 
 -- Yank to system regular clipboard
-map("v", "<C-c>", '"+y')
+map("v", "<C-c>", '"+y', { desc = "Yank to system clipboard" })
 
 -- Bind <Esc> to exit insert mode in terminal
 map("t", "<Esc>", "<C-\\><C-n>", { noremap = true })
 
 -- Quick buffer switching
 map("n", "<Leader><Tab>", ":bn<CR>", { desc = "Move to next buffer" })
+map("n", "<Leader><S-Tab>", ":bp<CR>", { desc = "Move to previous buffer" })
 
 -- Toggle line numbers on/off
 map("n", "<Leader>N", ":set invnumber<CR>", { desc = "Toggle line numbers" })
@@ -19,21 +20,21 @@ map("n", "Y", "y$", { desc = "Yank to the end of the line" })
 map("v", "<Leader>u", ":s/\\%V /_/g<CR>", { desc = "Replace spaces with underscores in selection" })
 
 -- DAP Bindings
-local dap = require("dap")
-map("n", "<Leader>dl", require("dap.ui.widgets").hover)
-map("n", "<Leader>dc", dap.continue)
-map("n", "<Leader>db", dap.toggle_breakpoint)
-map("n", "<Leader>dn", dap.step_over)
-map("n", "<Leader>di", dap.step_into)
-map("n", "<Leader>do", dap.step_out)
-map("n", "<Leader>dC", function()
-    dap.clear_breakpoints()
-    require("notify")("Breakpoints cleared", "warn")
-end)
-map("n", "<Leader>de", function()
-    dap.clear_breakpoints()
-    dap.terminate()
-end)
+-- local dap = require("dap")
+-- map("n", "<Leader>dl", require("dap.ui.widgets").hover)
+-- map("n", "<Leader>dc", dap.continue)
+-- map("n", "<Leader>db", dap.toggle_breakpoint)
+-- map("n", "<Leader>dn", dap.step_over)
+-- map("n", "<Leader>di", dap.step_into)
+-- map("n", "<Leader>do", dap.step_out)
+-- map("n", "<Leader>dC", function()
+--     dap.clear_breakpoints()
+--     require("notify")("Breakpoints cleared", "warn")
+-- end)
+-- map("n", "<Leader>de", function()
+--     dap.clear_breakpoints()
+--     dap.terminate()
+-- end)
 
 -- LSP bindings
 map("n", "<Space>,", vim.diagnostic.goto_prev, { desc = "Move to previous diagnostic" })
