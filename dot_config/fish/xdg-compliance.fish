ab6f312f62ce4974404b15613d4015782bad31dc
Pope_Rigby <poperigby@mailbox.org>
2023-10-19 20:27:26 -0700

    fish: fix mistake in xdg-compliance.fish

--- dot_config/fish/xdg-compliance.fish
+++ dot_config/fish/xdg-compliance.fish
@@ -11,10 +11,10 @@ set -x CARGO_HOME $XDG_DATA_HOME/cargo
 set -x GNUPGHOME  $XDG_DATA_HOME/gnupg
 
 # GTK2
-set -x GTK2_RC_FILES XDG_CONFIG_HOME/gtk-2.0/gtkrc
+set -x GTK2_RC_FILES $XDG_CONFIG_HOME/gtk-2.0/gtkrc
 
 # Less
-set -x LESSHISTFILE XDG_CACHE_HOME/less/history
+set -x LESSHISTFILE $XDG_CACHE_HOME/less/history
 
 # OpenJDK
 set -x _JAVA_OPTIONS -Djava.util.prefs.userRoot=$XDG_CONFIG_HOME/java
