b4a64aba6fa58f5ccae974aab2e5d72c352fedd0
Pope_Rigby <poperigby@mailbox.org>
2023-11-08 19:23:38 -0800

    neovim: set NormalFloat bg back to default

--- dot_config/nvim/lua/theming.lua
+++ dot_config/nvim/lua/theming.lua
@@ -4,7 +4,7 @@ require("gruvbox").setup({
     transparent_mode = false,
     overrides = {
         -- Make float backgrounds darker (and not transparent)
-        NormalFloat = { bg = "#282828" },
+        -- NormalFloat = { bg = "#282828" },
         -- Make incorrectly spelled words have a curly red underline
         SpellBad = { undercurl = true, sp = "#fb4934" },
     },
