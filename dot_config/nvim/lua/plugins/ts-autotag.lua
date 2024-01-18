f0c1d11531dba63bab3588feb1e5da203b0532f5
Pope_Rigby <poperigby@mailbox.org>
2024-01-18 10:35:09 -0800

    neovim: add nvim-ts-autotag

---
+++ dot_config/nvim/lua/plugins/ts-autotag.lua
@@ -0,0 +1,20 @@
+return {
+    "windwp/nvim-ts-autotag",
+    config = true,
+    ft     = {
+        "astro",
+        "glimmer",
+        "handlebars",
+        "html",
+        "javascript",
+        "jsx",
+        "markdown",
+        "php",
+        "rescript",
+        "svelte",
+        "tsx",
+        "typescript",
+        "vue",
+        "xml"
+    }
+}
