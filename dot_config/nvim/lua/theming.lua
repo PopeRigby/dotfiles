782e7e281cb5c32b750746b5aa20140b8e7c9b27
Pope_Rigby <poperigby@mailbox.org>
2024-01-29 11:53:50 -0800

    neovim: use underline for diagnostics

--- dot_config/nvim/lua/theming.lua
+++ dot_config/nvim/lua/theming.lua
@@ -6,9 +6,16 @@ require("gruvbox").setup({
     },
     overrides = {
         -- Make incorrectly spelled words have a curly red underline
-        SpellBad = { undercurl = true, sp = "#fb4934" },
+        SpellBad                 = { undercurl = true, sp = "#fb4934" },
+
+        -- Use straight underlines for diagnostics
+        DiagnosticUnderlineError = { underline = true, sp = "#fb4934" },
+        DiagnosticUnderlineWarn  = { underline = true, sp = "#fabd2f" },
+        DiagnosticUnderlineInfo  = { underline = true, sp = "#83a598" },
+        DiagnosticUnderlineHint  = { underline = true, sp = "#8ec07c" },
+
         -- Make whitespace easier to see
-        Whitespace = { fg = "#928374" }
+        Whitespace               = { fg = "#928374" },
     },
 })
 
